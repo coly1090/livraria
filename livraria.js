4a7c5c4c79d674ab57d61d205608b509f4eddce1
coly1090 <ferreira.emyly@escola.pr.gov.br>
2025-11-13 21:19:35 -0300

    Add files via upload

---
+++ livraria.js
@@ -0,0 +1,21 @@
+const livrosdisponiveis = [];
+
+
+
+livrosdisponiveis.push("bela e a fera");
+livrosdisponiveis.push("cinderela");
+livrosdisponiveis.push("a princesa e o sapo");
+livrosdisponiveis.push("bela adormecida");
+
+
+console.log("livros disponiveis:/n");
+console.log(livrosdisponiveis);
+
+
+
+
+livrosdisponiveis.push("coraçao de fero","o bicho vai pegar");
+
+
+console.log("livros disponiveis após atualização:");
+console.log(livrosdisponiveis);
